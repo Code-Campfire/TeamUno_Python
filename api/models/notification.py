485ec754965efc4aa84e200563ea85fb7459984b
Keaton Law <keaton.c.law@gmail.com>
2025-01-07 19:15:11 -0600

    added cascade to prevent orphan data, auto now for date fields

--- api/models/notification.py
+++ api/models/notification.py
@@ -7,8 +7,15 @@ from .post import Post
 class Notification(models.Model):
     post_id = models.ForeignKey(
         Post,
-        on_delete=models.DO_NOTHING
+        on_delete=models.CASCADE
     )
     content = models.CharField(max_length=150)
-    created_at = models.DateTimeField()
-    is_read = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    #should look into either deleting notifications after they are read, or maybe a periodic cleanup of old notifications. django post save could do it automatically, 
+    # or we could archive, but then we would still be storing lots of notification objects.
+    # @receiver(post_save, sender=Notification)
+    # def auto_delete_read_notifications(sender, instance, **kwargs):
+    #     if instance.is_read:
+    #         instance.delete()
