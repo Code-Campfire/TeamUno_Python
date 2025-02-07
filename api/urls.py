2c5d287f1bf6ba7e9cead1c8e681a0de80141263
Keaton Law <keaton.c.law@gmail.com>
2025-02-07 17:56:44 -0600

    auth viewset complete

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from .views.auth import AuthViewSet
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from api.views import PostViewSet, Profiles, Users, test_view #Edwin Moz added Post import
 
-router = routers.DefaultRouter(trailing_slash=False)
+router = DefaultRouter(trailing_slash=False)
 
 router.register(r"users", Users, "user")
 ## ///////////START CODE ADDED BY EDWIN MOZ ##
